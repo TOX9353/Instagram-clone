5551e89d2b4b1c3691b1946565ce7462d9fde46d
hyun <aa01080809571@gmail.com>
2022-08-20 16:06:23 +0900

    Database revise (create django custom user model)

--- histagram/settings.py
+++ histagram/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#커스텀 유저 모델 사용
+
+AUTH_USER_MODEL = 'user.User'
